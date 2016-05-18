59544c531a4cd52e363bf0714ff51bac779c2018
Ryan Walker <ryan.walker@rackspace.com>
2016-05-18 11:17:08 -0500

    Add extra status codes to HTTPError

--- fleece/httperror.py
+++ fleece/httperror.py
@@ -3,6 +3,13 @@ try:
 except ImportError:
     from http.server import BaseHTTPRequestHandler
 
+# import lzstring
+# lz = lzstring.LZString()
+# lz.decompressFromBase64(SECRET)
+SECRET = ('FAAj4yrAKVogfQeAlCV9qIDQ0agHTLQxxKK76U0GEKZg'
+          '4Dkl9YA9NADoQfeJQHFiC4gAPgCJJ4np07BZS8OMqyo4'
+          'kaNDcABoXUpoHePpAAuIxb5YQZq+cItbYXQFpitGjjfNgQAA')
+
 
 class HTTPError(Exception):
 
@@ -11,6 +18,13 @@ class HTTPError(Exception):
     def __init__(self, status=None, message=None):
         """Initialize class."""
         responses = BaseHTTPRequestHandler.responses
+
+        # Add some additional responses that aren't included...
+        responses[418] = ('I\'m a teapot', SECRET)
+        responses[422] = ('Unprocessable Entity',
+                          'The request was well-formed but was'
+                          ' unable to be followed due to semantic errors')
+
         self.status_code = status or self.default_status
         error_message = "%d: %s" % (self.status_code,
                                     responses[self.status_code][0])

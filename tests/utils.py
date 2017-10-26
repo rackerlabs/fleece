e3d27a19c9c7fe39108542d23d4d9e9cecece28e
Ryan Walker <ryan@wlkr.io>
2017-10-26 15:28:02 -0500

    Move testing auth data to reusable module

---
+++ tests/utils.py
@@ -0,0 +1,22 @@
+# This isn't a real token - go away.
+TEST_TOKEN = ('gAAAAABWq9MR30nr8Zx_-bgyAzBnWgFxnsxbup_cN01G7aoM66c7wEwz4r'
+              'QtTmniRVOhkVaF9a0Rt11IPMEqZ0mFk3bS7a4v4gAvmNa0-zE27UkuG58S'
+              'bRqe8zpyzRzyVEZsjR4fnza5')
+
+USER_DATA = {
+    'access': {
+        'token': {
+            'RAX-AUTH:authenticatedBy': ['PASSWORD'],
+            'expires': '2016-02-06T20:00:10.694Z',
+            'id': TEST_TOKEN,
+            'tenant': {
+                'id': '123456',
+                'name': '123456',
+            }
+        },
+        'user': {
+            'name': 'mytenantname',
+            'roles': [],
+        }
+    }
+}
